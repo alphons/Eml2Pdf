281df9df285e0b26c562b7ab829e4941988a8a74
alphons <alphons@heijden.com>
2025-02-20 17:35:57 +0100

    Add project files.

---
+++ Eml2Pdf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Eml2Pdf;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+			string emlPath = "input.eml";
+			string pdfPath = "output.pdf";
+
+			var email = Helper.ParseMultipartEml(emlPath);
+
+			Helper.CreatePdf(email, pdfPath);
+
+			Console.WriteLine("Conversie voltooid!");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Fout: {ex.Message}");
+		}
+	}
+
+
+}
