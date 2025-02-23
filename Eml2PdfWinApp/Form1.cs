90fdccd8edee33cb18cfc309c87df9a7b7b40652
alphons <alphons@heijden.com>
2025-02-23 16:12:45 +0100

    ok

--- Eml2PdfWinApp/Form1.cs
+++ Eml2PdfWinApp/Form1.cs
@@ -3,82 +3,81 @@ using MimePart2Pdf;
 using System.Diagnostics;
 using System.Text;
 
-namespace Eml2PdfWinApp
+namespace Eml2PdfWinApp;
+
+public partial class Form1 : Form
 {
-	public partial class Form1 : Form
+	public Form1()
 	{
-		public Form1()
+		InitializeComponent();
+	}
+
+	private async void Button1_Click(object sender, EventArgs e)
+	{
+		this.button1.Enabled = false;
+		this.textBox1.Enabled = false;
+		this.textBox2.Enabled = false;
+
+		Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+		foreach (var emlPath in Directory.GetFiles(this.textBox2.Text, "*.eml"))
 		{
-			InitializeComponent();
+			var name = Path.GetFileNameWithoutExtension(emlPath);
+
+			var pdfPath = Path.Combine(this.textBox3.Text, $"{name}.pdf");
+
+			var email = await MimePart.ReadEmlAsync(emlPath);
+
+			PdfHelper.CreatePdf(email, pdfPath);
+
 		}
 
-		private async void Button1_Click(object sender, EventArgs e)
+		this.textBox1.Enabled = true;
+		this.textBox2.Enabled = true;
+		this.button1.Enabled = true;
+	}
+
+	static async Task SaveMailAttachementsAsync(string OutputDir, MimePart part)
+	{
+		var contentType = part["Content-Type"];
+
+		if (part["Content-Disposition"].Contains("attachment"))
 		{
-			this.button1.Enabled = false;
-			this.textBox1.Enabled = false;
-			this.textBox2.Enabled = false;
-
-			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-			foreach (var emlPath in Directory.GetFiles(this.textBox2.Text, "*.eml"))
+			if (contentType.Contains("message/rfc822"))
 			{
-				var name = Path.GetFileNameWithoutExtension(emlPath);
-
-				var pdfPath = Path.Combine(this.textBox3.Text, $"{name}.pdf");
-
-				var email = await MimePart.ReadEmlAsync(emlPath);
-
-				PdfHelper.CreatePdf(email, pdfPath);
-
-			}
-
-			this.textBox1.Enabled = true;
-			this.textBox2.Enabled = true;
-			this.button1.Enabled = true;
-		}
-
-		static async Task SaveMailAttachementsAsync(string OutputDir, MimePart part)
-		{
-			var contentType = part["Content-Type"];
-
-			if (part["Content-Disposition"].Contains("attachment"))
-			{
-				if (contentType.Contains("message/rfc822"))
+				if (DateTime.TryParse(part.Parts[0]["Date"], out DateTime dtm))
 				{
-					if (DateTime.TryParse(part.Parts[0]["Date"], out DateTime dtm))
-					{
-						var outputPath = Path.Combine(OutputDir, $"{dtm:yyyyMMdd-HHmmss}.eml");
+					var outputPath = Path.Combine(OutputDir, $"{dtm:yyyyMMdd-HHmmss}.eml");
 
-						await part.SaveAsync(outputPath, dtm);
-					}
-					else
-					{
-						Debug.WriteLine("No Date found in message/rfc822 attachement");
-					}
+					await part.SaveAsync(outputPath, dtm);
+				}
+				else
+				{
+					Debug.WriteLine("No Date found in message/rfc822 attachement");
 				}
 			}
-
-			foreach (var subpart in part.Parts)
-			{
-				await SaveMailAttachementsAsync(OutputDir, subpart);
-			}
 		}
 
-
-		private async void Button2_Click(object sender, EventArgs e)
+		foreach (var subpart in part.Parts)
 		{
-			this.button2.Enabled = false;
-
-			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-			foreach (var emlPath in Directory.GetFiles(this.textBox1.Text, "*.eml"))
-			{
-				var email = await MimePart.ReadEmlAsync(emlPath);
-
-				await SaveMailAttachementsAsync(this.textBox2.Text, email);
-			}
-
-			this.button2.Enabled = true;
+			await SaveMailAttachementsAsync(OutputDir, subpart);
 		}
+	}
+
+
+	private async void Button2_Click(object sender, EventArgs e)
+	{
+		this.button2.Enabled = false;
+
+		Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+		foreach (var emlPath in Directory.GetFiles(this.textBox1.Text, "*.eml"))
+		{
+			var email = await MimePart.ReadEmlAsync(emlPath);
+
+			await SaveMailAttachementsAsync(this.textBox2.Text, email);
+		}
+
+		this.button2.Enabled = true;
 	}
 }
